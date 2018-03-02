fd8be5ce6c27cf325a07e52c87ec43c58155c7e6
Baidiuk Yevhen <y.baidiuk@gmail.com>
2018-03-02 21:19:54 +0100

    add some logik

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,24 +1,58 @@
 import React, {Component} from 'react';
 import '../App.css';
-import Difficulty from "./Difficulty";
+import {getDifficulty} from "../actions/ajaxRequests";
 
 
 class Body extends Component {
+
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            difficulty: '',
+            power: ''
+        };
+    }
+
+    componentDidMount() {
+        getDifficulty()
+            .done(data => {
+                this.setState({difficulty: data.difficulty})
+            })
+            .fail(() => {
+                this.setState({difficulty: "error"})
+            })
+    }
+
+    powerHandler(event) {
+        this.setState({power: event.target.value})
+    }
+
+    getProfit() {
+        return parseFloat(this.state.difficulty) * parseFloat(this.state.power);
+    }
+
     render() {
         return (
 
-                <div className="Body">
-                    <Difficulty/>
-                    <form>
-                        <label>
-                            Hashing Power:
-                            <input type="number" min="0" name="Th/s"/>
-                        </label>
-                    </form>
-                    profit
+            <div className="Body">
+                <div className="Difficulty">
+                    <p>
+                        Current Difficulty: {this.state.difficulty}
+                    </p>
 
+                    <label>
+                        Hashing Power: <input type="number"
+                                              value={this.state.power}
+                                              onChange={this.powerHandler.bind(this)}/>
+                    </label>
+                    <p>
+                        Profit : {this.getProfit()}
+                    </p>
 
                 </div>
+
+            </div>
         );
     }
 }
